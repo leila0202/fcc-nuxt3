773d4c7e3884df41eff9405c3c73037b6c8835bd
Leila Zrim <leilazrim@gmail.com>
2023-09-28 09:52:28 +0200

    added new sub component to the profile component and used it

---
+++ nuxt3-starter-new/components/Profile/Header/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        Profille Header
+    </div>
+</template>
--- nuxt3-starter-new/components/Profile/index.vue
+++ nuxt3-starter-new/components/Profile/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Profile component
+    <ProfileHeader />
   </div>
 </template>
 
