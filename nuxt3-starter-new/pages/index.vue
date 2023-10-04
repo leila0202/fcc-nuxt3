b39f7e11f4c23f980fbb58c254c915dd8f1a3c3b
Leila Zrim <leilazrim@gmail.com>
2023-10-04 13:54:00 +0200

    added a bell icon component

--- nuxt3-starter-new/pages/index.vue
+++ nuxt3-starter-new/pages/index.vue
@@ -2,9 +2,9 @@
 
 <template>
     <div class="Main">
-        Hello Nuxt 3
+        <h1>Hello Nuxt 3</h1>
     </div>
-    <img src="@/assets/img/2.jpg" alt="First image" />
+    <IconsBell/>
 </template>
 
 
